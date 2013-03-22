63f065e519d6e5398dabea78b5c60bd30a043af6
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-03-22 08:38:46 +0000

    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/social_integration/trunk@4428 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/control/EmailCallback.php
+++ code/control/EmailCallback.php
@@ -38,6 +38,9 @@ class EmailCallback extends SocialIntegrationControllerBaseClass implements Soci
 
 		//BODY
 		$body = $message;
+		if($link) {
+			$body .= "<br /><br />$link";
+		}
 
 		//CC
 		if(!empty($otherVariables["CC"])) {
