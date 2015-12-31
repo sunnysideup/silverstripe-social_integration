8380e68be6b886ea527d0bf5ee9ca23302fe2c72
helpfulrobot <chris@silverstripe.com>
2016-01-01 00:10:25 +1300

    Converted to PSR-2

--- code/model/FacebookIdentifier.php
+++ code/model/FacebookIdentifier.php
@@ -7,85 +7,91 @@
  *
  */
 
-class FacebookIdentifier extends DataObjectDecorator {
+class FacebookIdentifier extends DataObjectDecorator
+{
 
-	public function extraStatics() {
-		return array(
-			'db' => array(
-				'FacebookID' => 'Varchar',
-				'FacebookURL' => 'Varchar(255)',
-				'FacebookPicture' => 'Text',
-				'FacebookName' => 'Varchar(255)',
-				'FacebookEmail' => 'Varchar(255)',
-				'FacebookFirstName' => 'Varchar(255)',
-				'FacebookMiddleName' => 'Varchar(255)',
-				'FacebookLastName' => 'Varchar(255)',
-				'FacebookUsername' => 'Varchar(255)'
-			),
-			'indexes' => array(
-				'FacebookID' => true,
-				'FacebookUsername' => true
-			),
-			'casting' => array(
-				'FacebookButton' => 'ArrayData'
-			)
-		);
-	}
-
-
-	/**
-	 * connect and disconnect button
-	 * @return Object (IsConnected, Link, ConnectedName, ConnectedImageURL)
-	 */
-	public function getFacebookButton($backURL = "") {
-		return FacebookCallback::get_login_button($backURL, $this->owner);
-	}
-
-	/**
-	 *
-	 * user has logged in with facebook before?
-	 * @return Boolean
-	 */
-	public function hasFacebook() {
-		return (bool)$this->owner->FacebookID;
-	}
-
-	/**
-	 * user is currenctly connected to Faceboo
-	 *
-	 *
-	 */
-	public function isConnectedToFacebook() {
-		return FacebookCallback::get_current_user();
-	}
+    public function extraStatics()
+    {
+        return array(
+            'db' => array(
+                'FacebookID' => 'Varchar',
+                'FacebookURL' => 'Varchar(255)',
+                'FacebookPicture' => 'Text',
+                'FacebookName' => 'Varchar(255)',
+                'FacebookEmail' => 'Varchar(255)',
+                'FacebookFirstName' => 'Varchar(255)',
+                'FacebookMiddleName' => 'Varchar(255)',
+                'FacebookLastName' => 'Varchar(255)',
+                'FacebookUsername' => 'Varchar(255)'
+            ),
+            'indexes' => array(
+                'FacebookID' => true,
+                'FacebookUsername' => true
+            ),
+            'casting' => array(
+                'FacebookButton' => 'ArrayData'
+            )
+        );
+    }
 
 
-	/**
-	 * link to profile
-	 * @return String
-	 */
-	public function FacebookLink(){
-		if($this->owner->FacebookURL) {
-			return $this->owner->FacebookURL;
-		}
-		if($this->owner->FacebookID) {
-			return "http://www.facebook.com/people/@/".$this->owner->FacebookID;
-		}
-		return "";
-	}
+    /**
+     * connect and disconnect button
+     * @return Object (IsConnected, Link, ConnectedName, ConnectedImageURL)
+     */
+    public function getFacebookButton($backURL = "")
+    {
+        return FacebookCallback::get_login_button($backURL, $this->owner);
+    }
 
-	function onBeforeWrite(){
-		if(!$this->owner->Email) {
-			if($this->owner->FacebookEmail) {
-				$id = $this->owner->ID;
-				if(!$id) {
-					$id = 0;
-				}
-				if(!DataObject::get_one("Member", "\"Email\" = '".$this->owner->FacebookEmail."' AND \"Member\".\"ID\" <> ".$id."")) {
-					$this->owner->Email = $this->owner->FacebookEmail;
-				}
-			}
-		}
-	}
+    /**
+     *
+     * user has logged in with facebook before?
+     * @return Boolean
+     */
+    public function hasFacebook()
+    {
+        return (bool)$this->owner->FacebookID;
+    }
 
+    /**
+     * user is currenctly connected to Faceboo
+     *
+     *
+     */
+    public function isConnectedToFacebook()
+    {
+        return FacebookCallback::get_current_user();
+    }
+
+
+    /**
+     * link to profile
+     * @return String
+     */
+    public function FacebookLink()
+    {
+        if ($this->owner->FacebookURL) {
+            return $this->owner->FacebookURL;
+        }
+        if ($this->owner->FacebookID) {
+            return "http://www.facebook.com/people/@/".$this->owner->FacebookID;
+        }
+        return "";
+    }
+
+    public function onBeforeWrite()
+    {
+        if (!$this->owner->Email) {
+            if ($this->owner->FacebookEmail) {
+                $id = $this->owner->ID;
+                if (!$id) {
+                    $id = 0;
+                }
+                if (!DataObject::get_one("Member", "\"Email\" = '".$this->owner->FacebookEmail."' AND \"Member\".\"ID\" <> ".$id."")) {
+                    $this->owner->Email = $this->owner->FacebookEmail;
+                }
+            }
+        }
+    }
 }
